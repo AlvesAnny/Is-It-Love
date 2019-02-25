6825c9b8c58ae4a7824a7731383008affea25b53
Aluno <Aluno@DESKTOP-LDNVJ6K>
2019-02-25 08:19:09 -0300

    bhtegdgdnbgetegehngv bvjnbvh

---
+++ Projeto Is It Love/src/Personagem_Gabriel.java
@@ -0,0 +1,12 @@
+public class Personagem_Gabriel {
+
+	public int id;
+	public String nomeCompleto;
+	public int dataDeNascimento;
+	public String corDosOlhos;
+	public String Mundo;
+	public String serSobrenatural;
+	public String profissão;
+	public String parentesco;
+	public String personalidade;	
+}
---
+++ Projeto Is It Love/src/Personagem_Ryan.java
@@ -0,0 +1,13 @@
+public class Personagem_Ryan {
+
+	public int id;
+	public String nomeCompleto;
+	public int dataDeNascimento;
+	public String corDosOlhos;
+	public String Mundo;
+	public String serSobrenatural;
+	public String profissão;
+	public String parentesco;
+	public String personalidade;
+	
+}
